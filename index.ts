79cb0d17695a24d9495e03152a82fa14dec391ad
emiljohansson <emiljohansson.dev@gmail.com>
2022-12-28 10:36:20 -0700

    add eslint and prettier

--- index.ts
+++ index.ts
@@ -1,40 +1,36 @@
-const lettersPattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericPattern = "0123456789";
-const symbolsPattern = ",._?-:]&*#~}$>(<)@^|{%!+=";
-const maxLength = 1000;
+const lettersPattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numericPattern = "0123456789"
+const symbolsPattern = ",._?-:]&*#~}$>(<)@^|{%!+="
+const maxLength = 1000
 
 export default function randomString(props?: {
-  length?: number;
-  letters?: boolean;
-  numeric?: boolean;
-  symbols?: boolean;
+	length?: number
+	letters?: boolean
+	numeric?: boolean
+	symbols?: boolean
 }) {
-  const { length, letters, numeric, symbols } = {
-    length: 10,
-    letters: true,
-    numeric: true,
-    symbols: false,
-    ...props,
-  };
-  const pattern = [
-    letters && lettersPattern,
-    numeric && numericPattern,
-    symbols && symbolsPattern,
-  ]
-    .filter((v) => !!v)
-    .join("");
-  let value = "";
-  let index = length;
-  if (index > maxLength) {
-    index = maxLength;
-  }
+	const { length, letters, numeric, symbols } = {
+		length: 10,
+		letters: true,
+		numeric: true,
+		symbols: false,
+		...props,
+	}
+	const pattern = [letters && lettersPattern, numeric && numericPattern, symbols && symbolsPattern]
+		.filter((v) => !!v)
+		.join("")
+	let value = ""
+	let index = length
+	if (index > maxLength) {
+		index = maxLength
+	}
 
-  if (pattern.length < 1) return "";
+	if (pattern.length < 1) return ""
 
-  while (index--) {
-    const charIndex = Math.floor(Math.random() * pattern.length);
-    value += pattern[charIndex];
-  }
+	while (index--) {
+		const charIndex = Math.floor(Math.random() * pattern.length)
+		value += pattern[charIndex]
+	}
 
-  return value;
+	return value
 }
--- test.ts
+++ test.ts
@@ -1,21 +1,21 @@
-import test from "ava";
-import randomString from ".";
+import test from "ava"
+import randomString from "."
 
 test("validate length of string", (t) => {
-  let index = 20;
-  do {
-    t.is(
-      randomString({
-        length: index,
-      }).length,
-      index
-    );
-  } while (index-- && index > 0);
-  t.is(randomString().length, 10);
-  t.is(
-    randomString({
-      length: 1001,
-    }).length,
-    1000
-  );
-});
+	let index = 20
+	do {
+		t.is(
+			randomString({
+				length: index,
+			}).length,
+			index,
+		)
+	} while (index-- && index > 0)
+	t.is(randomString().length, 10)
+	t.is(
+		randomString({
+			length: 1001,
+		}).length,
+		1000,
+	)
+})
